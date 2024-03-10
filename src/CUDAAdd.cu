57e58cb32e4e23bfd2ef3540705235b9547ef350
l.undomiel <l.undomiel@outlook.com>
2024-03-10 08:36:02 +0800

    N线程块1线程 CUDAAdd

--- src/CUDAAdd.cu
+++ src/CUDAAdd.cu
@@ -1,8 +1,14 @@
 #include <iostream>
-#define N 100
+#define N 1024
 
 __global__ void CUDAAdd1(int a, int b, int *c) {
     *c = a + b;
+}
+
+__global__ void CUDAAdd2(int *a, int *b, int *c) {
+    int tid = blockIdx.x;
+    if (tid < N)
+        c[tid] = a[tid] + b[tid];
 }
 
 void CPUAdd(int *a, int *b, int *c) {
@@ -29,8 +35,7 @@ void test01() {
     std::cout << "2 + 7 = " << c << std::endl;
 }
 
-void test02()
-{
+void test02() {
     int a[N], b[N], c[N];
 
     // 赋值
@@ -46,8 +51,42 @@ void test02()
         printf("%d + %d = %d\n", a[i], b[i], c[i]);
 }
 
+void test03() {
+    int a[N], b[N], c[N];
+    int *dev_a, *dev_b, *dev_c;
+
+    // 赋值
+    for (int i = 0; i < N; i++) {
+        a[i] = -i + i * i;
+        b[i] = i * i * i;
+    }
+
+    // GPU上分配内存
+    cudaMalloc((void**)&dev_a, N * sizeof(int));
+    cudaMalloc((void**)&dev_b, N * sizeof(int));
+    cudaMalloc((void**)&dev_c, N * sizeof(int));
+
+    // 拷贝数据
+    cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
+    // GPU计算
+    // N个线程块,每个线程块1个线程
+    CUDAAdd2<<<N,1>>>(dev_a, dev_b, dev_c);
+
+    // 拷贝计算结果,释放GPU内存
+    cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_c);
+
+    for (int i = 0; i < N; i++)
+        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+}
+
 int main(int argc, char const *argv[]) {
-    test01();
-    test02();
+    // test01();
+    // test02();
+    test03();
     return 0;
 }
